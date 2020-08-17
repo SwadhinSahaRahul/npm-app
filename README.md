a19071070a6ca712efb805bef78f5fe5518d4e29
Swadhin Saha <rahul4kuet2k10@gmail.com>
2020-08-17 22:31:15 +0600

    shadowmaker

---
+++ README.md
@@ -0,0 +1,19 @@
+# What is this?
+
+Give shadows to images.
+
+# Installation
+```npm
+npm install shadowmaker
+```
+
+Then...
+
+```javascript
+import {shadowMaker} from 'shadowmaker';
+
+shadowMaker({
+    shadow_type : 'hard',
+    padding: true
+})
+```
---
+++ index.js
@@ -0,0 +1,20 @@
+const _ = require('lodash');
+
+function shadowMaker(options) {
+    let images = document.querySelectorAll('.shadow-maker');
+
+    if (options.shadow_type === 'hard')
+        options.shadow_type = '0px';
+    else
+        options.shadow_type = '15px';
+
+    _.each(images, (image) => {
+        image.style.boxShadow = `10px 10px ${options.shadow_type} 1px rgba(0, 0, 0, 0.12)`;
+        if (options.padding) {
+            image.style.padding = '1em'
+        }
+    })
+
+}
+
+export {shadowMaker}
