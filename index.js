fc191cd9c5a4528a0b893c42107cc97dc140b188
Swadhin Saha <rahul4kuet2k10@gmail.com>
2020-08-17 23:00:07 +0600

    typo removed

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function shadowMaker(options) {
         options.shadow_type = '15px';
 
     _.each(images, (image) => {
-        image.style.boxShadow = `10px 10px ${options.shadow_type} 1px rgba(0, 0, 0, 0.12)`;
+        image.style.boxShadow = `10px 10px ${options.shadow_type} 1px rgb(0, 0, 0, 0.12)`;
         if (options.padding) {
             image.style.padding = '1em'
         }
